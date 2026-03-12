d155d40e1887f5673e07c3f6e1d0b98958a09b09
Timipah <youremail@example.com>
2026-03-12 12:12:51 +0100

    fix react test to match UI

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Simple E-commerce title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/simple e-commerce/i);
+  expect(titleElement).toBeInTheDocument();
 });
